5099a42281b7d49a3f0ec54a3c13fc9a51e879b8
cumulus <148113369+devcumulus@users.noreply.github.com>
2024-01-15 18:35:18 +0900

    HOTFIX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
+import Breadcrumb from "./components/breadcrumb/Breadcrumb";
 import Loading from "./components/loading/Loading";
+// import "antd/dist/antd.css";
 
 const LazyJoinFirstPage = lazy(() => import("./pages/join/JoinFirstPage"));
 const LazyJoinPage = lazy(() => import("./pages/join/JoinPage"));
@@ -16,75 +17,88 @@ const LazyMainPage = lazy(() => import("./pages/main/MainPage"));
 
 const LazyPayPage = lazy(() => import("./pages/pay/PayPage"));
 
+const LazyDetailsPage = lazy(() => import("./pages/details/DetailsPage"));
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="main"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyMainPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/login"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyLoginPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/join/1"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyJoinFirstPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/join/2"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyJoinPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/join/3"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyJoinLastPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/my/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyMyPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/addedit/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyAddEditPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/pay/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyPayPage />
-            </Suspense>
-          }
-        ></Route>
-      </Routes>
+      <Breadcrumb />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="main"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyMainPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyLoginPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/join/1"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyJoinFirstPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/join/2"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyJoinPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/join/3"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyJoinLastPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/my/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyMyPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/addedit/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyAddEditPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/pay/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyPayPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/details/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyDetailsPage />
+              </Suspense>
+            }
+          ></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
