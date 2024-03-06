bb5580d6c1483a1cc3a19411e06100076899b58c
cumulus <148113369+devcumulus@users.noreply.github.com>
2024-03-06 19:36:35 +0900

    카카오 결제 연동중

--- src/api/login/kakao_api.js
+++ src/api/login/kakao_api.js
@@ -1,42 +1,42 @@
-// // import axios from "axios";
 // import axios from "axios";
-// import { SERVER_URL } from "../config";
-// // 앱 등록시 Rest 키 값(절대 오픈 금지)
-// const rest_api_key = "18b1120b1e489cb77a9ddc578db9af9a";
-// // 카카오 로그인 통과시 이동할 주소
-// const redirect_uri = "http://localhost:3000/login/kakao";
-// // 카카오 로그인 문서 참조
-// const auth_code_path = "https://kauth.kakao.com/oauth/authorize";
-// const response_type = "code"
-// const authParam = new URLSearchParams({
-//   rest_api_key, redirect_uri, response_type
-// })
-// // 카카오 로그인시 활용
-// export const getKakaoLoginLink = () => {
-//   const kakaoURL = `${auth_code_path}?${authParam.toString()}`;
-//   return kakaoURL;
-// };
-// // access 토큰 받기 경로
-// const access_token_url = `https://kauth.kakao.com/oauth/token`;
-// export const getAccessToken = async authCode => {
-//   const header = {
-//     headers: {
-//       "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-//     },
-//   };
-//   const params = {
+import axios from "axios";
+import { SERVER_URL } from "../config";
+// 앱 등록시 Rest 키 값(절대 오픈 금지)
+const rest_api_key = "18b1120b1e489cb77a9ddc578db9af9a";
+// 카카오 로그인 통과시 이동할 주소
+const redirect_uri = "http://localhost:3000/login/kakao";
+// 카카오 로그인 문서 참조
+const auth_code_path = "https://kauth.kakao.com/oauth/authorize";
+const response_type = "code";
+const authParam = new URLSearchParams({
+  rest_api_key,
+  redirect_uri,
+  response_type,
+});
+// 카카오 로그인시 활용
+export const getKakaoLoginLink = () => {
+  const kakaoURL = `${auth_code_path}?${authParam.toString()}`;
+  return kakaoURL;
+};
+// access 토큰 받기 경로
+const access_token_url = `https://kauth.kakao.com/oauth/token`;
+export const getAccessToken = async authCode => {
+  const header = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+  };
+  const params = {};
+  const res = await axios.post(access_token_url, params, header);
+  const accessToken = res.data.access_token;
+  return accessToken;
+};
+// Access Token 으로 회원정보 가져오기
+export const getMemberWithAccessToken = async accessToken => {
+  console.log("백엔드에 회원 등록을 위한 액세스 토큰 전달 ", accessToken);
+  const res = await axios.get(
+    `${SERVER_URL}/api/user/kakao?accessToken=${accessToken}`,
+  );
 
-//   };
-//   const res = await axios.post(access_token_url, params, header);
-//   const accessToken = res.data.access_token;
-//   return accessToken;
-// };
-// // Access Token 으로 회원정보 가져오기
-// export const getMemberWithAccessToken = async accessToken => {
-//     console.log("백엔드에 회원 등록을 위한 액세스 토큰 전달 ", accessToken);
-//     const res = await axios.get(
-//       `${SERVER_URL}/api/user/kakao?accessToken=${accessToken}`,
-//     );
-
-//     return res.data;
-//   };
+  return res.data;
+};
